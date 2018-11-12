ddb5e73aefc00a6d07c737bf83093b0105cb7b87
Gaurav Sethi <gaurav@netscapelabs.com>
2018-11-12 13:26:17 +0530

    push origin

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -33,11 +33,12 @@ class UserController extends Controller {
     }
 
     public function googleLogin(Request $request) {
-
-        $request->session()->put('filename', $this->filename);
-        $request->session()->put('fileurl', $this->fileurl);
-
-//            dd($this);
+        if ($this->filename):
+            $request->session()->put('filename', $this->filename);
+        endif;
+        if ($this->fileurl):
+            $request->session()->put('fileurl', $this->fileurl);
+        endif;
         $google_oauthV2 = new \Google_Service_Oauth2($this->gClient);
         if ($request->get('code')) {
             $this->gClient->authenticate($request->get('code'));
@@ -47,50 +48,50 @@ class UserController extends Controller {
             $this->gClient->setAccessToken($request->session()->get('token'));
         }
         if ($this->gClient->getAccessToken()) {
-            //For logged in user, get details from google using acces
-//            $user = User::find(1);
-//            $user->access_token = json_encode($request->session()->get('token'));
-//            $user->save();
-//            dd("Successfully authenticated");
-            dd($request->session()->get('filename'));
             $this->uploadFileUsingAccessToken($request);
         } else {
             //For Guest user, get google login url
+            echo 'Uploading data to Google drive';
             $authUrl = $this->gClient->createAuthUrl();
             return redirect()->to($authUrl);
         }
     }
 
     public function uploadFileUsingAccessToken(Request $request) {
-        dd($request->session()->get('fileurl'));
-        $service = new \Google_Service_Drive($this->gClient);
-//        $user = User::find(1);
-        $userToken = $request->session()->get('token');
-        $this->gClient->setAccessToken($userToken);
-        if ($this->gClient->isAccessTokenExpired()) {
-            // save refresh token to some variable
-            $refreshTokenSaved = $this->gClient->getRefreshToken();
-            // update access token
-            $this->gClient->fetchAccessTokenWithRefreshToken($refreshTokenSaved);
-            // // pass access token to some variable
-            $updatedAccessToken = $this->gClient->getAccessToken();
-            // // append refresh token
-            $updatedAccessToken['refresh_token'] = $refreshTokenSaved;
-            //Set the new acces token
-            $this->gClient->setAccessToken($updatedAccessToken);
-            $request->session()->put('token', $updatedAccessToken);
-        }
-        $fileMetadata = new \Google_Service_Drive_DriveFile(array('name' => 'ExpertPHP', 'mimeType' => 'application/vnd.google-apps.folder'));
-        $folder = $service->files->create($fileMetadata, array('fields' => 'id'));
+        $staus = true;
+        try {
+            $service = new \Google_Service_Drive($this->gClient);
+            $userToken = $request->session()->get('token');
+            $this->gClient->setAccessToken($userToken);
+            if ($this->gClient->isAccessTokenExpired()) {
+                // save refresh token to some variable
+                $refreshTokenSaved = $this->gClient->getRefreshToken();
+                // update access token
+                $this->gClient->fetchAccessTokenWithRefreshToken($refreshTokenSaved);
+                // // pass access token to some variable
+                $updatedAccessToken = $this->gClient->getAccessToken();
+                // // append refresh token
+                $updatedAccessToken['refresh_token'] = $refreshTokenSaved;
+                //Set the new acces token
+                $this->gClient->setAccessToken($updatedAccessToken);
+                $request->session()->put('token', $updatedAccessToken);
+            }
+            $fileMetadata = new \Google_Service_Drive_DriveFile(array('name' => 'ExpertPHP', 'mimeType' => 'application/vnd.google-apps.folder'));
+            $folder = $service->files->create($fileMetadata, array('fields' => 'id'));
 //        printf("Folder ID: %s\n", $folder->id);
-        $file = new \Google_Service_Drive_DriveFile(['name' => $request->session()->get('filename'), 'parents' => array($folder->id)]);
-
-        dd($request->session()->get('fileUrl'));
-        $result = $service->files->create($file, ['data' => file_get_contents($request->session()->get('fileUrl')), 'mimeType' => 'application/octet-stream', 'uploadType' => 'media']);
-        // get url of uploaded file
-//        dd($result);
-        // echo $url = 'https://drive.google.com/open?id=' . $result->id;
-        return ($result->id) ? $result->id : false;
+            $file = new \Google_Service_Drive_DriveFile(['name' => $request->session()->get('filename'), 'parents' => array($folder->id)]);
+            $result = $service->files->create($file, ['data' => file_get_contents($request->session()->get('fileurl')), 'mimeType' => 'application/octet-stream', 'uploadType' => 'media']);
+            // get url of uploaded file
+            $url = 'https://drive.google.com/open?id=' . $result->id;
+            \Session::forget('filename');
+            \Session::forget('fileurl');
+            die('s');
+            return view('share.google', compact('request'));
+        } catch (\Exception $ex) {
+            $staus = false;
+            dd($ex->getMessage());
+            return view('share.google', compact('staus'));
+        }
     }
 
 }
