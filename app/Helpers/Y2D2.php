be13bd38c31753cc6b606614694e54b160bd4f88
Ubuntu <ubuntu@ip-172-31-36-140.us-east-2.compute.internal>
2018-11-14 14:35:46 +0000

    conflicted branch

---
+++ app/Helpers/Y2D2.php
@@ -0,0 +1,279 @@
+<?php
+
+namespace App\Helpers;
+
+class Y2D2 {
+
+    public static function getFileSize($url) {
+        $bytes = self::remoteFile_size($url);
+        return self::size_to_string($bytes);
+    }
+
+    public static function youtube_url_to_id($url) {
+        $values = '';
+        if (preg_match('/youtube\.com\/watch\?v=([^\&\?\/]+)/', $url, $id)) {
+            $values = $id[1];
+        } else if (preg_match('/youtube\.com\/embed\/([^\&\?\/]+)/', $url, $id)) {
+            $values = $id[1];
+        } else if (preg_match('/youtube\.com\/v\/([^\&\?\/]+)/', $url, $id)) {
+            $values = $id[1];
+        } else if (preg_match('/youtu\.be\/([^\&\?\/]+)/', $url, $id)) {
+            $values = $id[1];
+        } else if (preg_match('/youtube\.com\/verify_age\?next_url=\/watch%3Fv%3D([^\&\?\/]+)/', $url, $id)) {
+            $values = $id[1];
+        }
+        return $values;
+    }
+
+    public static function size_to_string($bytes) {
+        if ($bytes >= 1073741824) {
+            $bytes = number_format($bytes / 1073741824, 2) . ' GB';
+        } elseif ($bytes >= 1048576) {
+            $bytes = number_format($bytes / 1048576, 2) . ' MB';
+        } elseif ($bytes >= 1024) {
+            $bytes = number_format($bytes / 1024, 2) . ' KB';
+        } elseif ($bytes > 1) {
+            $bytes = $bytes . ' bytes';
+        } elseif ($bytes == 1) {
+            $bytes = $bytes . ' byte';
+        } else {
+            $bytes = 'N/A';
+        }
+        return $bytes;
+    }
+
+    public static function mp3_size($kbps, $duration) {
+        if (isset($kbps) && isset($duration)) {
+            $size = $kbps * $duration / 8 * 1000;
+            return size_to_string($size);
+        }
+        return false;
+    }
+
+    public static function destroy($dir, $life = 3600) {
+        if (is_dir($dir)) {
+            $objects = scandir($dir);
+            foreach ($objects as $object) {
+                if ($object != "." && $object != "..") {
+                    if (filetype($dir . "/" . $object) == "dir")
+                        destroy($dir . "/" . $object, $life);
+                    else if (time() - filemtime($dir . "/" . $object) > $life) {
+                        @unlink($dir . "/" . $object);
+                        echo $dir . "/" . $object . ' - Deleted<br>';
+                    }
+                }
+            }
+//        rmdir($dir);
+//        echo $dir . ' - Deleted<br>';
+        }
+    }
+
+    public static function remoteFile_size($url) {
+        $data = get_headers($url, true);
+        if (isset($data['Content-Length']))
+            return (int) $data['Content-Length'];
+    }
+
+    public static function sec_to_time($seconds) {
+        return gmdate("H:i:s", $seconds);
+    }
+
+    public static function to_ascii($str, $options = array(), $delimiter = ' ') {
+        // Make sure string is in UTF-8 and strip invalid UTF-8 characters
+        $str = mb_convert_encoding((string) $str, 'UTF-8', mb_list_encodings());
+        $defaults = array(
+            'delimiter' => $delimiter,
+            'limit' => null,
+            'lowercase' => false,
+            'replacements' => array(),
+            'transliterate' => false
+        );
+
+        if (!isset($options) && $options == '') {
+            $options = array();
+        }
+        // Merge options
+        $options = array();
+        $options = array_merge($defaults, $options);
+
+        $char_map = array(
+            // Latin
+            'À' => 'A', 'Á' => 'A', 'Â' => 'A', 'Ã' => 'A', 'Ä' => 'A', 'Å' => 'A', 'Æ' => 'AE', 'Ç' => 'C',
+            'È' => 'E', 'É' => 'E', 'Ê' => 'E', 'Ë' => 'E', 'Ì' => 'I', 'Í' => 'I', 'Î' => 'I', 'Ï' => 'I',
+            'Ð' => 'D', 'Ñ' => 'N', 'Ò' => 'O', 'Ó' => 'O', 'Ô' => 'O', 'Õ' => 'O', 'Ö' => 'O', 'Ő' => 'O',
+            'Ø' => 'O', 'Ù' => 'U', 'Ú' => 'U', 'Û' => 'U', 'Ü' => 'U', 'Ű' => 'U', 'Ý' => 'Y', 'Þ' => 'TH',
+            'ß' => 'ss',
+            'à' => 'a', 'á' => 'a', 'â' => 'a', 'ã' => 'a', 'ä' => 'a', 'å' => 'a', 'æ' => 'ae', 'ç' => 'c',
+            'è' => 'e', 'é' => 'e', 'ê' => 'e', 'ë' => 'e', 'ì' => 'i', 'í' => 'i', 'î' => 'i', 'ï' => 'i',
+            'ð' => 'd', 'ñ' => 'n', 'ò' => 'o', 'ó' => 'o', 'ô' => 'o', 'õ' => 'o', 'ö' => 'o', 'ő' => 'o',
+            'ø' => 'o', 'ù' => 'u', 'ú' => 'u', 'û' => 'u', 'ü' => 'u', 'ű' => 'u', 'ý' => 'y', 'þ' => 'th',
+            'ÿ' => 'y',
+            // Latin symbols
+            '©' => '(c)',
+            // Greek
+            'Α' => 'A', 'Β' => 'B', 'Γ' => 'G', 'Δ' => 'D', 'Ε' => 'E', 'Ζ' => 'Z', 'Η' => 'H', 'Θ' => '8',
+            'Ι' => 'I', 'Κ' => 'K', 'Λ' => 'L', 'Μ' => 'M', 'Ν' => 'N', 'Ξ' => '3', 'Ο' => 'O', 'Π' => 'P',
+            'Ρ' => 'R', 'Σ' => 'S', 'Τ' => 'T', 'Υ' => 'Y', 'Φ' => 'F', 'Χ' => 'X', 'Ψ' => 'PS', 'Ω' => 'W',
+            'Ά' => 'A', 'Έ' => 'E', 'Ί' => 'I', 'Ό' => 'O', 'Ύ' => 'Y', 'Ή' => 'H', 'Ώ' => 'W', 'Ϊ' => 'I',
+            'Ϋ' => 'Y',
+            'α' => 'a', 'β' => 'b', 'γ' => 'g', 'δ' => 'd', 'ε' => 'e', 'ζ' => 'z', 'η' => 'h', 'θ' => '8',
+            'ι' => 'i', 'κ' => 'k', 'λ' => 'l', 'μ' => 'm', 'ν' => 'n', 'ξ' => '3', 'ο' => 'o', 'π' => 'p',
+            'ρ' => 'r', 'σ' => 's', 'τ' => 't', 'υ' => 'y', 'φ' => 'f', 'χ' => 'x', 'ψ' => 'ps', 'ω' => 'w',
+            'ά' => 'a', 'έ' => 'e', 'ί' => 'i', 'ό' => 'o', 'ύ' => 'y', 'ή' => 'h', 'ώ' => 'w', 'ς' => 's',
+            'ϊ' => 'i', 'ΰ' => 'y', 'ϋ' => 'y', 'ΐ' => 'i',
+            // Turkish
+            'Ş' => 'S', 'İ' => 'I', 'Ç' => 'C', 'Ü' => 'U', 'Ö' => 'O', 'Ğ' => 'G',
+            'ş' => 's', 'ı' => 'i', 'ç' => 'c', 'ü' => 'u', 'ö' => 'o', 'ğ' => 'g',
+            // Russian
+            'А' => 'A', 'Б' => 'B', 'В' => 'V', 'Г' => 'G', 'Д' => 'D', 'Е' => 'E', 'Ё' => 'Yo', 'Ж' => 'Zh',
+            'З' => 'Z', 'И' => 'I', 'Й' => 'J', 'К' => 'K', 'Л' => 'L', 'М' => 'M', 'Н' => 'N', 'О' => 'O',
+            'П' => 'P', 'Р' => 'R', 'С' => 'S', 'Т' => 'T', 'У' => 'U', 'Ф' => 'F', 'Х' => 'H', 'Ц' => 'C',
+            'Ч' => 'Ch', 'Ш' => 'Sh', 'Щ' => 'Sh', 'Ъ' => '', 'Ы' => 'Y', 'Ь' => '', 'Э' => 'E', 'Ю' => 'Yu',
+            'Я' => 'Ya',
+            'а' => 'a', 'б' => 'b', 'в' => 'v', 'г' => 'g', 'д' => 'd', 'е' => 'e', 'ё' => 'yo', 'ж' => 'zh',
+            'з' => 'z', 'и' => 'i', 'й' => 'j', 'к' => 'k', 'л' => 'l', 'м' => 'm', 'н' => 'n', 'о' => 'o',
+            'п' => 'p', 'р' => 'r', 'с' => 's', 'т' => 't', 'у' => 'u', 'ф' => 'f', 'х' => 'h', 'ц' => 'c',
+            'ч' => 'ch', 'ш' => 'sh', 'щ' => 'sh', 'ъ' => '', 'ы' => 'y', 'ь' => '', 'э' => 'e', 'ю' => 'yu',
+            'я' => 'ya',
+            // Ukrainian
+            'Є' => 'Ye', 'І' => 'I', 'Ї' => 'Yi', 'Ґ' => 'G',
+            'є' => 'ye', 'і' => 'i', 'ї' => 'yi', 'ґ' => 'g',
+            // Czech
+            'Č' => 'C', 'Ď' => 'D', 'Ě' => 'E', 'Ň' => 'N', 'Ř' => 'R', 'Š' => 'S', 'Ť' => 'T', 'Ů' => 'U',
+            'Ž' => 'Z',
+            'č' => 'c', 'ď' => 'd', 'ě' => 'e', 'ň' => 'n', 'ř' => 'r', 'š' => 's', 'ť' => 't', 'ů' => 'u',
+            'ž' => 'z',
+            // Polish
+            'Ą' => 'A', 'Ć' => 'C', 'Ę' => 'e', 'Ł' => 'L', 'Ń' => 'N', 'Ó' => 'o', 'Ś' => 'S', 'Ź' => 'Z',
+            'Ż' => 'Z',
+            'ą' => 'a', 'ć' => 'c', 'ę' => 'e', 'ł' => 'l', 'ń' => 'n', 'ó' => 'o', 'ś' => 's', 'ź' => 'z',
+            'ż' => 'z',
+            // Latvian
+            'Ā' => 'A', 'Č' => 'C', 'Ē' => 'E', 'Ģ' => 'G', 'Ī' => 'i', 'Ķ' => 'k', 'Ļ' => 'L', 'Ņ' => 'N',
+            'Š' => 'S', 'Ū' => 'u', 'Ž' => 'Z',
+            'ā' => 'a', 'č' => 'c', 'ē' => 'e', 'ģ' => 'g', 'ī' => 'i', 'ķ' => 'k', 'ļ' => 'l', 'ņ' => 'n',
+            'š' => 's', 'ū' => 'u', 'ž' => 'z',
+        );
+
+        // Make custom replacements
+        $str = preg_replace(array_keys($options['replacements']), $options['replacements'], $str);
+
+        // Transliterate characters to ASCII
+        if ($options['transliterate']) {
+            $str = str_replace(array_keys($char_map), $char_map, $str);
+        }
+
+        // Replace non-alphanumeric characters with our delimiter
+        $str = preg_replace('/[^\p{L}\p{Nd}]+/u', $options['delimiter'], $str);
+
+        // Remove duplicate delimiters
+        $str = preg_replace('/(' . preg_quote($options['delimiter'], '/') . '){2,}/', '$1', $str);
+
+        // Truncate slug to max. characters
+        $str = mb_substr($str, 0, ($options['limit'] ? $options['limit'] : mb_strlen($str, 'UTF-8')), 'UTF-8');
+
+        // Remove delimiter from ends
+        $str = trim($str, $options['delimiter']);
+
+        return $options['lowercase'] ? $str : $str;
+    }
+
+    public static function ffmpeg_progress($file) {
+        $content = @file_get_contents($file);
+        if ($content) {
+            //get duration of source
+            preg_match("/Duration: (.*?), start:/", $content, $matches);
+
+            $rawDuration = $matches[1];
+
+            //rawDuration is in 00:00:00.00 format. This converts it to seconds.
+            $ar = array_reverse(explode(":", $rawDuration));
+            $duration = floatval($ar[0]);
+            if (!empty($ar[1]))
+                $duration += intval($ar[1]) * 60;
+            if (!empty($ar[2]))
+                $duration += intval($ar[2]) * 60 * 60;
+
+            //get the time in the file that is already encoded
+            preg_match_all("/time=(.*?) bitrate/", $content, $matches);
+
+            $rawTime = array_pop($matches);
+
+            //this is needed if there is more than one match
+            if (is_array($rawTime)) {
+                $rawTime = array_pop($rawTime);
+            }
+
+            //rawTime is in 00:00:00.00 format. This converts it to seconds.
+            $ar = array_reverse(explode(":", $rawTime));
+            $time = floatval($ar[0]);
+            if (!empty($ar[1]))
+                $time += intval($ar[1]) * 60;
+            if (!empty($ar[2]))
+                $time += intval($ar[2]) * 60 * 60;
+
+            //calculate the progress
+            $progress = round(($time / $duration) * 100);
+
+            return $progress;
+            //echo "Duration: " . $duration . "<br>";
+            //  echo "Current Time: " . $time . "<br>";
+            // echo "Progress: " . $progress . "%";
+        } else {
+            return 0;
+        }
+    }
+
+    public static function make_comparer() {
+        // Normalize criteria up front so that the comparer finds everything tidy
+        $criteria = func_get_args();
+        foreach ($criteria as $index => $criterion) {
+            $criteria[$index] = is_array($criterion) ? array_pad($criterion, 3, null) : array($criterion, SORT_ASC, null);
+        }
+        return function ($first, $second) use (&$criteria) {
+            foreach ($criteria as $criterion) {
+                // How will we compare this round?
+                list($column, $sortOrder, $projection) = $criterion;
+                $sortOrder = $sortOrder === SORT_DESC ? -1 : 1;
+                // If a projection was defined project the values now
+                if ($projection) {
+                    $lhs = call_user_func($projection, $first[$column]);
+                    $rhs = call_user_func($projection, $second[$column]);
+                } else {
+                    $lhs = $first[$column];
+                    $rhs = $second[$column];
+                }
+                // Do the actual comparison; do not return if equal
+                if ($lhs < $rhs) {
+                    return -1 * $sortOrder;
+                } else if ($lhs > $rhs) {
+                    return 1 * $sortOrder;
+                }
+            }
+            return 0; // tiebreakers exhausted, so $first == $second
+        };
+    }
+
+    public static function tweakUrl($url, $title) {
+        $pattern = "/^(https:\/\/)[a-zA-Z0-9-.]+.googlevideo.com\//";
+        $url_return = preg_replace($pattern, 'https://redirector.googlevideo.com/', $url);
+        return $url_return . '&title=' . $title;
+    }
+
+    public static function array_orderby() {
+        $args = func_get_args();
+        $data = array_shift($args);
+        foreach ($args as $n => $field) {
+            if (is_string($field)) {
+                $tmp = array();
+                foreach ($data as $key => $row)
+                    $tmp[$key] = $row[$field];
+                $args[$n] = $tmp;
+            }
+        }
+        $args[] = &$data;
+        call_user_func_array('array_multisort', $args);
+        return array_pop($args);
+    }
+
+}
