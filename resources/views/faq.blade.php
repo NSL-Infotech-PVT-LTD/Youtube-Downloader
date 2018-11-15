3bc1d5f12bd4de98ccc7747a6df830ed75b8860b
netscape-vinay <vinay@netscapelabs.com>
2018-11-15 19:16:06 +0530

    localization for other pages

--- resources/views/faq.blade.php
+++ resources/views/faq.blade.php
@@ -4,8 +4,8 @@
 	<section class="content-area faq">
 		<div class="container">
 		<div class="row banner content">
-				<h1 class="heading">Frequently Asked <span>Questions</span></h1>
-				<p class="sub-heading">Do you have aquestion? Check answer below!</p>
+				<h1 class="heading">{{__('faq.frequently_asked') }}<span>{{__('faq.questions') }}</span></h1>
+				<p class="sub-heading">{{__('faq.do_you') }}</p>
 			<div class="col-md-7">				
   
   <div class="panel-group" id="accordion">
@@ -14,13 +14,13 @@
     <div class="panel-heading">
       <h4 class="panel-title">
         <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#collapseOne">
-          <h5>Collapsible Group Item #1</h5>
+          <h5>{{__('faq.Collapsible_1') }}</h5>
         </a>
       </h4>
     </div>
     <div id="collapseOne" class="panel-collapse collapse in">
 	    <div class="panel-body">
-        <p>Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.</p>
+        <p>{{__('faq.content') }}</p>
       </div>
     </div>
   </div>
@@ -28,13 +28,13 @@
     <div class="panel-heading">
       <h4 class="panel-title">
         <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo">
-          <h5>Collapsible Group Item #2</h5>
+          <h5>{{__('faq.Collapsible_2') }}</h5>
         </a>
       </h4>
     </div>
     <div id="collapseTwo" class="panel-collapse collapse">
       <div class="panel-body">
-        <p>Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.</p>
+        <p>{{__('faq.content') }}</p>
       </div>
     </div>
   </div>
@@ -42,13 +42,13 @@
     <div class="panel-heading">
       <h4 class="panel-title">
         <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#collapseThree">
-          <h5>Collapsible Group Item #3</h5>
+          <h5>{{__('faq.Collapsible_3') }}</h5>
         </a>
       </h4>
     </div>
     <div id="collapseThree" class="panel-collapse collapse">
       <div class="panel-body">
-        <p>Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.</p>
+        <p>{{__('faq.content') }}</p>
       </div>
     </div>
   </div>
@@ -57,13 +57,13 @@
     <div class="panel-heading">
       <h4 class="panel-title">
         <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#collapse4">
-          <h5>Collapsible Group Item #4</h5>
+          <h5>{{__('faq.Collapsible_4') }}</h5>
         </a>
       </h4>
     </div>
     <div id="collapse4" class="panel-collapse collapse">
       <div class="panel-body">
-        <p>Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.</p>
+        <p>{{__('faq.content') }}</p>
       </div>
     </div>
   </div>
@@ -71,13 +71,13 @@
     <div class="panel-heading">
       <h4 class="panel-title">
         <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#collapse5">
-         <h5> Collapsible Group Item #5</h5>
+         <h5>{{__('faq.Collapsible_5') }}</h5>
         </a>
       </h4>
     </div>
     <div id="collapse5" class="panel-collapse collapse">
       <div class="panel-body">
-        <p>Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.</p>
+        <p>{{__('faq.content') }}</p>
       </div>
     </div>
   </div>
@@ -85,13 +85,13 @@
     <div class="panel-heading">
       <h4 class="panel-title">
         <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#collapse6">
-          <h5>Collapsible Group Item #6</h5>
+          <h5>{{__('faq.Collapsible_6') }}</h5>
         </a>
       </h4>
     </div>
     <div id="collapse6" class="panel-collapse collapse">
       <div class="panel-body">
-        <p>Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.</p>
+        <p>{{__('faq.content') }}</p>
       </div>
     </div>
   </div>
@@ -99,13 +99,13 @@
     <div class="panel-heading">
       <h4 class="panel-title">
         <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#collapse7">
-          <h5>Collapsible Group Item #7</h5>
+          <h5>{{__('faq.Collapsible_7') }}</h5>
         </a>
       </h4>
     </div>
     <div id="collapse7" class="panel-collapse collapse">
       <div class="panel-body"><p>
-        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.</p>
+       {{__('faq.content') }}</p>
       </div>
     </div>
   </div>
@@ -117,11 +117,11 @@
 			<div class="col-md-5">	
 				
 				<div class="help-inner panel panel-default">
-				<h5><i class="fa fa-question-circle"></i> Helps</h5>
-				<p>Can't find the answer.<br>
-				We are here to help?</p>				
-				<h5><i class="fa fa-comment-o"></i> Ask question in <a href="">Forum page</a></h5>
-				<h5><i class="fa fa-envelope-o"></i> Contact us by <a href="">Forum page</a>	</h5>
+				<h5><i class="fa fa-question-circle"></i> {{__('faq.helps') }}</h5>
+				<p> {{__('faq.cant_find') }}<br>
+				{{__('faq.we_are_here') }}</p>				
+				<h5><i class="fa fa-comment-o"></i> {{__('faq.ask_question_in') }} <a href="">Forum page</a></h5>
+				<h5><i class="fa fa-envelope-o"></i>{{__('faq.contact_us_by') }}<a href="">{{__('faq.form_page') }}</a>	</h5>
 				</div>
 			</div>
 		</div>	
--- resources/views/terms-of-use.blade.php
+++ resources/views/terms-of-use.blade.php
@@ -5,20 +5,20 @@
     <div class="container">
         <div class="row banner content">
             <div class="col-md-12">	
-                <h1  class="heading"><span>Term</span> of use</h1>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                <h1  class="heading"><span>{{__('termofuse.term') }}</span>{{__('termofuse.of_use') }}</h1>
+                <p>{{__('termofuse.content') }}</p>
 
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-                <h5>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</h5>
+                <p>{{__('termofuse.content') }}</p>
+                <h5>{{__('termofuse.lorem_ipsum') }}</h5>
                 <ul>
-                    <li>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</li>
-                    <li>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</li>
-                    <li>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</li>
-                    <li>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</li>
-                    <li>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</li>
+                    <li>{{__('termofuse.lorem_ipsum') }}</li>
+                    <li>{{__('termofuse.lorem_ipsum') }}</li>
+                    <li>{{__('termofuse.lorem_ipsum') }}</li>
+                    <li>{{__('termofuse.lorem_ipsum') }}</li>
+                    <li>{{__('termofuse.lorem_ipsum') }}</li>
                 </ul>
 
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing.</p>
+                <p>{{__('termofuse.content') }}</p>
 
             </div>
         </div>	
